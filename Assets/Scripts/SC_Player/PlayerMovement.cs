09c16f9f79ce54bbca96ed5593d0606966570c7c
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-01 00:48:09 -0300

    Set character animations & rotation

--- Assets/Scripts/SC_Player/PlayerMovement.cs
+++ Assets/Scripts/SC_Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 10;
     [SerializeField] Transform movePoint;
-
+    [SerializeField] Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +20,34 @@ public class PlayerMovement : MonoBehaviour
         
         if(Vector3.Distance(transform.position, movePoint.position) <= 0.05 && Input.anyKeyDown )
         {
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+            float horizontalAxisValue = Input.GetAxisRaw("Horizontal");
+            float verticalAxisValue = Input.GetAxisRaw("Vertical");
+            if(Mathf.Abs(horizontalAxisValue) == 1)
             {
+                if(horizontalAxisValue < 0)
+                    transform.rotation = Quaternion.Euler(0, -90, 0);
+                else
+                    transform.rotation = Quaternion.Euler(0, 90, 0);
                 //HACER ESTO UN ACTION
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") * 2, 0, 0);
+                anim.SetBool("IsMoving",true);
+                movePoint.position += new Vector3(horizontalAxisValue * 2, 0, 0);
             }
 
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            if (Mathf.Abs(verticalAxisValue) == 1)
             {
+                if (verticalAxisValue < 0)
+                    transform.rotation = Quaternion.Euler(0, 180, 0);
+                else
+                    transform.rotation = Quaternion.identity;
+
                 //HACER ESTO UN ACTION
+                anim.SetBool("IsMoving",true);
                 movePoint.position += new Vector3(0, 0, Input.GetAxisRaw("Vertical") * 2);
             }
+        }
+        else if(Vector3.Distance(transform.position, movePoint.position) <= 0.05)
+        {
+            anim.SetBool("IsMoving",false);
         }
     }
 }
