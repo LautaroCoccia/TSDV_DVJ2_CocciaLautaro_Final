7c2330cf7f6fb5cd64442e1f4307bb6bd4ca056c
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-01 21:51:41 -0300

    Add vehicles movement

---
+++ Assets/Scripts/SC_Player/MovePointController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePointController : MonoBehaviour
+{
+    Vector3 prevPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SetPrevPos(Vector3 newPrevPos)
+    {
+        prevPos = newPrevPos;
+    }
+    private void OnCollisionEnter(Collision other) {
+        
+    }
+}
---
+++ Assets/Scripts/SC_Vehicles/SC_VehicleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_VehicleController : MonoBehaviour
+{
+    [SerializeField] float velocity;
+    Vector3 startPosition;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate() 
+    {
+        rb.velocity = velocity * Vector3.right;
+        Debug.Log("VELOCITY" + velocity * Vector3.right);
+        Debug.Log("--------------");
+        Debug.Log("RB.VELOCITY" + rb.velocity);
+        Debug.Log("--------------");
+    }
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log(other.transform.name);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        transform.position = startPosition;    
+    }
+}
