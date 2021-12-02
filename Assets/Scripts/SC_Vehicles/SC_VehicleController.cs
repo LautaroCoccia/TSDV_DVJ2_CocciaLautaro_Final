db2f73387c590158605b5370019331da0b5b8cc8
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-01 22:41:29 -0300

    Fix vehicles movement

--- Assets/Scripts/SC_Vehicles/SC_VehicleController.cs
+++ Assets/Scripts/SC_Vehicles/SC_VehicleController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class SC_VehicleController : MonoBehaviour
 {
     [SerializeField] float velocity;
+    enum directionEnum
+    {
+        left,
+        right
+    }
+    [SerializeField] directionEnum direction;
     Vector3 startPosition;
     Rigidbody rb;
     // Start is called before the first frame update
@@ -21,18 +27,27 @@ public class SC_VehicleController : MonoBehaviour
     }
     void FixedUpdate() 
     {
-        rb.velocity = velocity * Vector3.right;
-        Debug.Log("VELOCITY" + velocity * Vector3.right);
-        Debug.Log("--------------");
-        Debug.Log("RB.VELOCITY" + rb.velocity);
-        Debug.Log("--------------");
+        rb.velocity = velocity * GetDirection();
     }
-    private void OnCollisionEnter(Collision other) {
-        Debug.Log(other.transform.name);
+    private void OnCollisionEnter(Collision other) 
+    {
+    
     }
 
     private void OnTriggerEnter(Collider other) 
     {
         transform.position = startPosition;    
+    }
+
+    Vector3 GetDirection()
+    {
+        if(direction == directionEnum.left)
+        {
+            return Vector3.left;
+        }
+        else
+        {
+            return Vector3.right;
+        }
     }
 }
